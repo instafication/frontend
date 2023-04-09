c215246c43a8f3e15277b9b1893ebbd43932ad1e
Nopsled <78918436+Nopsled@users.noreply.github.com>
2023-04-09 23:44:14 +0200

    Update

--- src/lib/Managers/EmailManager.ts
+++ src/lib/Managers/EmailManager.ts
@@ -7,7 +7,44 @@ interface Parameters {
 
 
 export class EmailManager {
+
     private static readonly API_URL = 'https://api.sendinblue.com/v3/smtp/email';
+
+    public static async SendEmailWhenUserIsCreated(to: string, body: string): Promise<boolean> {
+        const requestBody = JSON.stringify({
+            to: [{ email: to, name: "Blinksms" }],
+            // sender: { "name": "Blinksms", "email": "Blinksms <hej@blinksms.se>" },
+            templateId: 9,
+            params: {
+                "message": body,
+            },
+        });
+
+        try {
+            const response = await fetch(EmailManager.API_URL, {
+                method: 'POST',
+                headers: {
+                    "accept": "application/json",
+                    "Content-Type": "application/json",
+                    "api-key": "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",
+                },
+                body: requestBody,
+            });
+
+            if (!response.ok) {
+                const errorText = await response.text();
+                console.error(
+                    `[Emailmanager, -] Error sending email: ${response.status} ${response.statusText}\n\n${errorText}`,
+                );
+                return false;
+            }
+
+            return true;
+        } catch (error) {
+            console.error('[Emailmanager, -] Error sending email:', error.message);
+            return false;
+        }
+    }
 
     public static async sendEmail(
         to: string,
