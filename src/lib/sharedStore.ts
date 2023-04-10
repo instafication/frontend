036b7d5e465477b655b23bdf938ee0185a8f2734
Nopsled <78918436+Nopsled@users.noreply.github.com>
2023-04-10 18:15:58 +0200

    U

--- src/lib/sharedStore.ts
+++ src/lib/sharedStore.ts
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 
 export let showInformationModal = writable<boolean>(false);
-export let showLoginModal = writable<boolean>(true);
+export let showLoginModal = writable<boolean>(false);
 export let showRegisterModal = writable<boolean>(false);
 export let showServicesModal = writable<boolean>(false);
 export let showLostPasswordModal = writable<boolean>(false);
