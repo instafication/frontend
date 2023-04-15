ecf9745266cb7d1c872ba994165312a5db8e0ffe
Nopsled <78918436+Nopsled@users.noreply.github.com>
2023-04-15 19:37:43 +0200

    U

--- src/lib/Components/HeaderPhone.svelte
+++ src/lib/Components/HeaderPhone.svelte
@@ -6,38 +6,9 @@
 <div class="bg-white">
 
   <div class="relative isolate pt-0">
-      <!-- <div class="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl" aria-hidden="true">
-    <div class="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-  </div> -->
-    <svg class="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]" aria-hidden="true">
-      <defs>
-        <!-- <pattern id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
-          <path d="M100 200V.5M.5 .5H200" fill="none" />
-        </pattern> -->
-      </defs>
-      <svg x="50%" y="-1" class="overflow-visible fill-gray-50">
-        <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
-      </svg>
-      <!-- <rect width="100%" height="100%" stroke-width="0" fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" /> -->
-    </svg>
+
     <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
       <div class="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
-
-        <!-- <div class="flex">
-
-          <div class="relative flex items-center gap-x-4 rounded-full px-4 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            <span class="font-semibold text-indigo-600">We’re hiring</span>
-            <span class="h-4 w-px bg-gray-900/10" aria-hidden="true"></span>
-            <a href="#" class="flex items-center gap-x-1">
-              <span class="absolute inset-0" aria-hidden="true"></span>
-              See open positions
-              <svg class="-mr-2 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
-              </svg>
-            </a>
-          </div>
-
-        </div> -->
 
         <h1 class="mt-10 max-w-lg text-6xl font-bold tracking-tight text-gray-900 sm:text-6xl">{$t("header_title")}</h1>
         <p class="mt-6 text-lg leading-8 text-gray-600">
