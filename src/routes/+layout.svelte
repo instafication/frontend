a23cf97c50e8b943a947e1f66ea8eed7ac9ab8f0
Martin <Hello@martinjakobsson.se>
2025-07-30 00:26:39 +0200

    Update package.json version and dependencies; remove unused PostCSS and Prisma configurations; refactor Svelte and Vite configurations for improved compatibility with Tailwind CSS.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
-
-  import "../app.postcss";
+  
+  import "../app.css";
   import { isLoggedIn, supabase } from "$lib/Managers/AuthManager";
-  import { onMount } from 'svelte';
-  import { selectedLanguage, userLoggedIn } from '$lib/sharedStore';
-  import { page } from '$app/stores';
-  import HeaderLoggedIn from '$lib/Components/HeaderLoggedIn.svelte';
-  import Header from '$lib/Components/Header.svelte';
-  import Footer from '$lib/Components/Footer.svelte';
+  import { onMount } from "svelte";
+  import { selectedLanguage, userLoggedIn } from "$lib/sharedStore";
+  import { page } from "$app/stores";
+  import HeaderLoggedIn from "$lib/Components/HeaderLoggedIn.svelte";
+  import Header from "$lib/Components/Header.svelte";
+  import Footer from "$lib/Components/Footer.svelte";
   import ModalLogin from "$lib/Components/Modal/ModalLogin.svelte";
   import ModalLostPassword from "$lib/Components/Modal/ModalLostPassword.svelte";
   import ModalServices from "$lib/Components/Modal/ModalServices.svelte";
   import ModalRegister from "$lib/Components/Modal/ModalRegister.svelte";
   import ProfileSettingsModal from "$lib/Components/Modal/Profile/ProfileSettingsModal.svelte";
   import type { RequestEvent } from "@sveltejs/kit";
-  import { inject } from '@vercel/analytics';
+  import { inject } from "@vercel/analytics";
 
   // Vercel Analytics.
   inject();
-
 
   let lastAuthStatus = "";
   let lastSession = null;
@@ -26,38 +25,33 @@
   onMount(async () => {
     //selectedLanguage.set($page.data.language);
     $userLoggedIn = await isLoggedIn();
-  })
+  });
 
   supabase.auth.onAuthStateChange(async (event, session) => {
-      
     lastAuthStatus = event;
     lastSession = session;
 
     //console.log("event", event)
     //console.log("session", session);
 
-    if (event == 'SIGNED_OUT') {
+    if (event == "SIGNED_OUT") {
       $userLoggedIn = false;
-    } else if (event == 'SIGNED_IN') {
+    } else if (event == "SIGNED_IN") {
       $userLoggedIn = true;
     }
-    
-  })
-
+  });
 </script>
 
-
-<ModalLogin/>
-<ModalRegister/>
-<ModalLostPassword/>
-<ModalServices/>
-<ProfileSettingsModal/>
-
+<ModalLogin />
+<ModalRegister />
+<ModalLostPassword />
+<ModalServices />
+<ProfileSettingsModal />
 
 {#if $userLoggedIn}
-  <HeaderLoggedIn/>
+  <HeaderLoggedIn />
 {:else}
   <Header />
 {/if}
 <slot></slot>
-<Footer/>
+<Footer />
