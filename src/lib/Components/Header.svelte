8e64fe727b8404f6dd6c2381b6e570f1133cfd00
Martin <Hello@martinjakobsson.se>
2025-07-30 02:01:34 +0200

    Update svelte-preprocess dependency to version ^6.0.3; modify footer text in translations; refactor Header component for improved structure and readability; remove unused blinksms-frontend workspace file; fix import statement in layout file.

--- src/lib/Components/Header.svelte
+++ src/lib/Components/Header.svelte
@@ -1,80 +1,69 @@
 <script lang="ts">
+  import {
+    Navbar,
+    NavBrand,
+    NavHamburger,
+    NavUl,
+    NavLi,
+    Button
+  } from 'flowbite-svelte';
 
-  import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, Button} from 'flowbite-svelte'
-  import { showLoginModal, showRegisterModal } from '$lib/sharedStore'
+  import {
+    showLoginModal,
+    showRegisterModal
+  } from '$lib/sharedStore';
+
   import LanguageSelector from './LanguageSelector.svelte';
   import { t } from '$lib/i18n';
-
 </script>
 
-<!-- <ModalLogin bind:showLoginModal />
-<ModalRegister bind:showRegisterModal/> -->
-
-<Navbar let:hidden let:toggle>
+<Navbar>
+  <!-- logo / brand -->
   <NavBrand href="/">
-
-    <!-- <lord-icon
-    src="https://cdn.lordicon.com/alnsmmtf.json"
-    trigger="hover"
-    colors="primary:#4be1ec,secondary:#cb5eee"
-    stroke="90"
-    style="width:64px;height:64px">
-    </lord-icon> -->
-
-<!-- <lord-icon
-    src="https://cdn.lordicon.com/tyounuzx.json"
-    trigger="hover"
-    colors="primary:#121331,secondary:#3080e8"
-    stroke="90"
-    style="width:64px;height:64px">
-</lord-icon> -->
-
-
-    <img src="/images/logo.png" class="mr-3 h-6 sm:h-9" alt="Instafication Logo"/>
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Instafication</span>
+    <img src="/images/logo.png" class="mr-3 h-6 sm:h-9" alt="Instafication Logo" />
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      Instafication
+    </span>
   </NavBrand>
 
-    <NavHamburger on:click={toggle} />
+  <!-- mobile hamburger -->
+  <NavHamburger />
 
-  <NavUl {hidden} class="order-1">
-    <NavLi href="#" on:click="{() => $showLoginModal = true}">
+  <!-- main nav links -->
+  <NavUl class="order-1">
+    <!-- log-in -->
+    <NavLi href="#" on:click={() => ($showLoginModal = true)}>
       <Button color="alternative">
-		      <lord-icon
+        <lord-icon
           src="https://cdn.lordicon.com/rqqkvjqf.json"
           trigger="hover"
-          colors="primary:#121331,secondary:#12133"
+          colors="primary:#121331"
           stroke="40"
           scale="40"
-          style="width:32px;height:32px">
-      </lord-icon>
-      {$t("log_in")}
-      <script src="https://cdn.lordicon.com/ritcuqlt.js"></script>
-      <script src="https://cdn.lordicon.com/ritcuqlt.js"></script>
-    </Button>
-  </NavLi>
+          style="width:32px;height:32px"
+        />
+        {$t('log_in')}
+      </Button>
+    </NavLi>
 
-
-
-  <NavLi href="#" on:click="{() => $showRegisterModal = true}">
-    <Button color="blue">
-			    <lord-icon
+    <!-- register -->
+    <NavLi href="#" on:click={() => ($showRegisterModal = true)}>
+      <Button color="blue">
+        <lord-icon
           src="https://cdn.lordicon.com/wcjauznf.json"
           trigger="hover"
-          colors="primary:#FFF,secondary:#FFF"
+          colors="primary:#FFF"
           stroke="50"
           scale="40"
-          style="width:32px;height:32px">
-      </lord-icon>
-      {$t('register')}
-	</Button>
-  </NavLi>
+          style="width:32px;height:32px"
+        />
+        {$t('register')}
+      </Button>
+    </NavLi>
 
-
-	<NavLi href="#" class="divide-y">
-		<LanguageSelector/>
-	</NavLi>
-
-
+    <!-- language selector -->
+    <NavLi class="divide-y">
+      <LanguageSelector />
+    </NavLi>
   </NavUl>
-
 </Navbar>
