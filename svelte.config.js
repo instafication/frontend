303a60680e314d92cef8443f7e8b09f30349dda8
Martin <Hello@martinjakobsson.se>
2025-08-02 04:48:42 +0200

    Update project configuration and dependencies for improved functionality
    - Modified package.json to include the `--minify` flag in the deploy script and added `clsx` as a new dependency.
    - Updated TypeScript configuration to include custom types for better type safety.
    - Enabled Svelte preprocess in svelte.config.js for enhanced functionality.
    - Cleaned up app.css by removing unnecessary whitespace.
    - Updated app.html structure for better readability.
    - Enhanced database manager by importing additional SQL functions for improved query capabilities.

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,29 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-  // preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
   compilerOptions: {
     runes: true
   },
   kit: {
     adapter: adapter({
       // See below for an explanation of these options
-      config: undefined,
-      platformProxy: {
-        configPath: undefined,
-        environment: undefined,
-        persist: undefined
-      },
-      fallback: 'plaintext',
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>']
-      }
+      // config: undefined,
+      // platformProxy: {
+      //   configPath: undefined,
+      //   environment: undefined,
+      //   persist: undefined
+      // },
+      // fallback: 'plaintext',
+      // routes: {
+      //   include: ['/*'],
+      //   exclude: ['<all>']
+      // }
     }),
-    alias: {
-      "@/*": "./src/lib/*",
-    },
+    // alias: {
+    //   "@/*": "./src/lib/*",
+    // },
   }
 };
